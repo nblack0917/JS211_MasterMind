4c0a4cfcea43f1d98af392ff482007d569091269
nblack0917 <nickablack@gmail.com>
2020-11-05 14:01:50 -0600

    fixed small log issues

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 let guessNum  = 0;
+let gameOver = false;
 
 const printBoard = () =>  {
   for (let i = 0; i < board.length; i++) {
@@ -68,22 +69,32 @@ const mastermind = (guess) => {
   // solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
   if (guess == solution) {
+    console.log(" ")
     console.log('You guessed it!')
+    console.log(" ")
     return 'You guessed it!'
-  } else if (board.length < 10) {
+  } else if (board.length < 9) {
+    console.log(" ")
     console.log(`Guess again.`)
+    console.log(" ")
   } else {
-    console.log(`You ran out of turns! The solution was ${solution}. Otherwise, return 'Guess again.`)
+    console.log(" ")
+    console.log(`You ran out of turns! The solution was ${solution}.`)
+    console.log(" ")
+    gameOver = true
   }
-  console.log(generateHint(guess))
+  generateHint(guess)
 }
 
 const getPrompt = () =>  {
-  rl.question('guess: ', (guess) => {
-    mastermind(guess);
-    printBoard();
-    getPrompt();
-  });
+  if (gameOver === false) {
+    console.log("Your options are: a, b, c, d, e, f, g, and h")
+    rl.question('guess: ', (guess) => {
+      mastermind(guess);
+      printBoard();
+      getPrompt();
+    });
+}
 }
 
 // Tests
